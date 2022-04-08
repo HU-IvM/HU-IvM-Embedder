5eb0d355323197b3a13f60a3bbd4d81070a98733
Ronald Broekhuizen <github@ronaldbroekhuizen.nl>
2022-04-08 21:29:23 +0200

    Update huvim-embedder.php

--- huvim-embedder.php
+++ huvim-embedder.php
@@ -10,34 +10,6 @@
 	Text Domain: huivm-embedder
 */
 
-/* Plugin Setup */
-/**
- * Access this plugin’s working instance
- *
- * @wp-hook plugins_loaded
- * @return  object of this class
- */
-add_filter( 'upgrader_source_selection', array( $this, 'rename_github_zip' ), 1, 3);
-
-/**
- * Removes the prefix "-master" when updating from GitHub zip files
- * 
- * See: https://github.com/YahnisElsts/plugin-update-checker/issues/1
- * 
- * @param string $source
- * @param string $remote_source
- * @param object $thiz
- * @return string
- */
-public function rename_github_zip( $source, $remote_source, $thiz )
-{
-    $path_parts = pathinfo( $source );
-    $newsource = trailingslashit( $path_parts['dirname'] ) . trailingslashit( 'huivm-embedder' );
-    rename( $source, $newsource );
-	echo $newsource;
-    return $newsource;
-}
-
 /* Update Checker */
 require 'plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
